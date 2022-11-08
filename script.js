09255b9570c1063c3301a6d1aea59b717f8984d7
SeydeSaw <115941526+SeydeSaw@users.noreply.github.com>
2022-11-08 20:55:42 +0100

    08.11.22

---
+++ script.js
@@ -0,0 +1,146 @@
+
+//____________________Домашка 1 _______________________
+
+
+let r = 90
+let g = 50
+let b = 210  
+console.log("rgb(" + r + ", " + g + ", " + b + ")" )
+
+
+//____________________Домашка 2 _______________________
+
+
+let number = +prompt("Введите число")
+if(number > 100) { alert("Больше") }
+else if(number == 100) { alert("Равны") }
+else { alert("Меньше") }
+console.log(alert)
+
+//____________________Домашка 3 ____________________
+
+
+let number1 = +prompt("Введите первое число")
+let number2 = +prompt("Введите второе число")
+let а = number1 + number2
+let б = number1 - number2
+let в = number1 * number2
+let г = number1 / number2
+
+console.log("сумма)" + а ,"разность)" + б ,"произведение)" + в ,"деление)" + г )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert("Hello World")
+//console.log("Hello Arsen")
+
+//alert(username)
+//console.log(username)
+//username = "Ruslan"
+//let userName = "Arsen"
+//let filteredUserProduct = "Product"
+//____________________________________________
+//Задаём переменной значение онлайн с помощью prompt:
+//let username = prompt("Введите своё имя")
+
+//Задаём переменной значение
+//(вводим в переменную определённые данные):
+//let hello = "Hello "
+//____________________________________________
+// Выводим в консоль данные:
+//1.let hello = "Hello "
+//let username = prompt("Введите своё имя")
+//console.log(hello + username)
+//2.let username = prompt("Введите своё имя")
+//console.log("Hello " + username)
+
+//console.log(hello + "World")
+
+//____________________________________________
+//Условный оператор.Если что-то сделал,то вышел результат
+//Наприме: Если идёт дождь, нужно взять зонт, ИНАЧЕ "Погода сегодня солнечная" 
+
+//let isRaining = false
+//if(isRaining == true) {
+//alert("Возьми зонт") //выполняется только если условие верно
+//}
+//else {
+//alert("Погода сегодня солнечная")
+//}
+//___
+
+//let degree = 20
+//if(degree > 15) {
+//    alert("Выходи в футболке")
+//}
+//else if(degree < 3) {
+//    alert("Надеть куртку")
+//}
+//else {
+//    alert("Надень кофту")
+//}
+//______________________________________________
+
+//let price = 100
+//if(price >= 700) {
+//   alert("Твоя скидка 20%")
+//}
+//else if(price >= 500) {
+//   alert("Твоя скидка 15%")
+//}
+//else {
+//  alert("У Вас нет скидки")
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
